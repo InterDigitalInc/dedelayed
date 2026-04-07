7173eb8897b8f2bedf491c2db9748d5c175101c0
Mateen Ulhaq <mateen.ulhaq@interdigital.com>
2026-04-06 21:17:23 -0700

    refactor: dedelayed.registry.DATASETS

--- dedelayed/registry.py
+++ dedelayed/registry.py
@@ -2,14 +2,31 @@
 # All rights reserved.
 # See LICENSE under the root folder.
 
-from typing import Callable, Dict, Type, TypeVar
+from __future__ import annotations
+
+from typing import Type, TypeVar
 
 import torch.nn as nn
+from torch.utils.data import Dataset
 
+TDataset = Dataset
 TModel = nn.Module
+
+TDataset_b = TypeVar("TDataset_b", bound=TDataset)
 TModel_b = TypeVar("TModel_b", bound=TModel)
 
-MODELS: Dict[str, Callable[..., TModel]] = {}
+DATASETS: dict[str, Type[TDataset]] = {}
+MODELS: dict[str, Type[TModel]] = {}
+
+
+def register_dataset(name: str):
+    """Decorator for registering a dataset."""
+
+    def decorator(cls: Type[TDataset_b]) -> Type[TDataset_b]:
+        DATASETS[name] = cls
+        return cls
+
+    return decorator
 
 
 def register_model(name: str):
