f775f4e998537607924e24bdc06790580ef27c96
Mateen Ulhaq <mateen.ulhaq@interdigital.com>
2026-04-23 22:11:34 -0700

    fix: reintroduce min_lr_ratio=1e-4

--- dedelayed/utils/optim.py
+++ dedelayed/utils/optim.py
@@ -16,12 +16,15 @@ class RaisedCosineLR(torch.optim.lr_scheduler.LambdaLR):
         *,
         num_training_steps: int,
         lr_pow: int = 2,
+        min_lr_ratio: float = 1e-4,
         last_epoch: int = -1,
     ) -> None:
         self.num_training_steps = num_training_steps
         self.lr_pow = lr_pow
+        self.min_lr_ratio = min_lr_ratio
         super().__init__(optimizer, self._lr_lambda, last_epoch=last_epoch)
 
     def _lr_lambda(self, current_step: int) -> float:
         t = min(current_step / self.num_training_steps, 1.0)
-        return 1.0 - math.cos(math.pi * t) ** (2 * self.lr_pow)
+        scale = 1.0 - math.cos(math.pi * t) ** (2 * self.lr_pow)
+        return self.min_lr_ratio + (1.0 - self.min_lr_ratio) * scale
